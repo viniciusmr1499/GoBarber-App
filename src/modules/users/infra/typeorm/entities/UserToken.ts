e1c78f50b504c685c5b578a8e0101cfa689408ef
Marcos Vinicius <viniciusmr1499@gmail.com>
2020-10-26 21:35:34 -0300

    adicionado nova funcionalidade de recuperar senha

---
+++ src/modules/users/infra/typeorm/entities/UserToken.ts
@@ -0,0 +1,26 @@
+import {
+    Entity,
+    Column,
+    PrimaryGeneratedColumn,
+    UpdateDateColumn,
+    CreateDateColumn,
+    Generated
+} from 'typeorm';
+
+@Entity('user_tokens')
+class UserToken {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column()
+    @Generated('uuid')
+    token: string;
+
+    @CreateDateColumn()
+    created_at: Date;
+
+    @UpdateDateColumn()
+    updated_at: Date;
+}
+
+export default UserToken;
--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -3,7 +3,9 @@ import { inject, injectable } from 'tsyringe';
 // import AppError from '@shared/errors/AppError';
 
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
+import IUserTokensRepository from '@modules/users/repositories/IUserTokensRepository';
 import IMailProvider from '@shared/container/providers/MailProvider/models/ImailProvider';
+import AppError from '@shared/errors/AppError';
 
 
 // import User from '@modules/users/infra/typeorm/entities/User';
@@ -20,9 +22,20 @@ class SendForgotPasswordEmailService{
 
         @inject('MailProvider')
         private mailProvider: IMailProvider,
+
+        @inject('UserTokensRepository')
+        private userTokensRepository: IUserTokensRepository,
     ) {}
 
     public async execute({ email }: RequestDTO):Promise<void> {
+        const user = await this.usersRepository.findByEmail(email);
+
+        if(!user) {
+            throw new AppError('User do not exists.');
+        }
+
+        await this.userTokensRepository.generate(user.id)
+
         this.mailProvider.sendMail(email, 'Pedido de recuperação de senha recebido.');
     }
 
