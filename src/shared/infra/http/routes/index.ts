f1689e425d29a111c8d5797ccd3e25c7ca59eb83
Marcos Vinicius <viniciusmr1499@gmail.com>
2020-10-27 21:20:19 -0300

    adicionando migration user_tokens no app

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -3,11 +3,13 @@ import { Router } from 'express';
 import appointments from '@modules/appointments/infra/http/routes/appointments.routes';
 import sessions from '@modules/users/infra/http/routes/sessions.routes';
 import users from '@modules/users/infra/http/routes/user.routes';
+import passwords from '@modules/users/infra/http/routes/password.routes';
 
 const routes = Router();
 
 routes.use('/appointments', appointments);
 routes.use('/users', users);
 routes.use('/sessions', sessions);
+routes.use('/password', passwords);
 
 export default routes;
