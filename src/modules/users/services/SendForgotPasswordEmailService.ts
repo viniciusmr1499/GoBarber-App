84320bf2c6731016e860218fb4fdfc7b95d75dd9
Marcos Vinicius <viniciusmr1499@gmail.com>
2020-11-01 11:58:43 -0300

    finalizado template-engine

--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -1,4 +1,5 @@
 import { inject, injectable } from 'tsyringe';
+import path from 'path';
 
 import AppError from '@shared/errors/AppError';
 
@@ -30,7 +31,9 @@ class SendForgotPasswordEmailService{
             throw new AppError('User do not exists.');
         }
 
-        const { token } = await this.userTokensRepository.generate(user.id)
+        const { token } = await this.userTokensRepository.generate(user.id);
+
+        const forgotPasswordTemplate = path.resolve(__dirname, '..', 'views', 'forgot_password.hbs');
 
         await this.mailProvider.sendMail({
             to: {
@@ -39,10 +42,10 @@ class SendForgotPasswordEmailService{
             },
             subject: '[GoBarber] Recuperação de senha',
             templateData: {
-                template: 'Olá, {{name}}: {{token}}',
+                file: forgotPasswordTemplate,
                 variables: {
                     name: user.name,
-                    token,
+                    link: `http://localhost:3000/reset_password?token=${token}`,
                 },
             },
         });
