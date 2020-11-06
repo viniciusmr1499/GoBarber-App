5585bd51d4d595991ac16b32676b80d36f748680
Marcos Vinicius <viniciusmr1499@gmail.com>
2020-11-06 20:54:57 -0300

    excluindo horarios disponiveis

--- src/modules/appointments/services/ListProviderDayAvailabilityService.ts
+++ src/modules/appointments/services/ListProviderDayAvailabilityService.ts
@@ -1,5 +1,5 @@
 import { injectable, inject } from 'tsyringe';
-import { getHours } from 'date-fns';
+import { getHours, isAfter } from 'date-fns';
 
 import IApointmentsRepository from '@modules/appointments/repositories/IApointmentsRepository';
 
@@ -32,17 +32,24 @@ export default class ListProviderDayAvailabilityService {
 
         const hourStart = 8;
 
-        const eachHourArray = Array.from({ length: 10 }, (_, index) => index + hourStart );
+        const eachHourArray = Array.from(
+            { length: 10 },
+            (_, index) => index + hourStart,
+        );
+
+        const currentDate = new Date(Date.now());
 
         const availability = eachHourArray.map(hour => {
             const hasAppointmentInHour = appointments.find(appointment =>
                 getHours(appointment.date) === hour,
             );
 
+            const compareDate = new Date(year, month - 1, day, hour);
+
             return {
                 hour,
-                available: !hasAppointmentInHour
-            }
+                available: !hasAppointmentInHour && isAfter(compareDate, currentDate),
+            };
         })
 
         return availability;
