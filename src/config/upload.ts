5dfe198e661e0615f018a935d5940efdcff33fc2
Marcos Vinicius <viniciusmr1499@gmail.com>
2020-11-17 22:00:22 -0300

    adicionando configuracoes de cdn

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -1,20 +1,49 @@
+import multer, { StorageEngine } from 'multer';
 import path from 'path';
-import multer from 'multer';
 import crypto from 'crypto';
 
 const tmpFolder = path.resolve(__dirname, '..', '..', 'tmp');
 
+interface IUploadConfig {
+    driver: 's3' | 'disk';
+
+    tmpFolder: string;
+    uploadsFolder: string;
+
+    multer: {
+        storage: StorageEngine;
+    };
+
+    config: {
+        disk: {},
+        aws: {
+            bucket: string;
+        }
+    };
+}
+
 export default {
+    driver: process.env.SORAGE_DRIVER,
+
     tmpFolder,
     uploadsFolder: path.resolve(tmpFolder, 'uploads'),
 
-    storage: multer.diskStorage({
-        destination: tmpFolder,
-        filename(request, file, callback){
-            const fileHash = crypto.randomBytes(10).toString('hex');
-            const filename = `${fileHash}-${file.originalname}`;
+    multer: {
+        storage: multer.diskStorage({
+            destination: tmpFolder,
+            filename(request, file, callback){
+                const fileHash = crypto.randomBytes(10).toString('hex');
+                const filename = `${fileHash}-${file.originalname}`;
 
-            return callback(null, filename);
+                return callback(null, filename);
+            },
+        }),
+    },
+
+    config: {
+        disk: {},
+        aws: {
+            bucket: 'app-gobarber-2',
         },
-    }),
-};
+    },
+} as IUploadConfig;
