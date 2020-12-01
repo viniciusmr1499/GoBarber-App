de00c2e50c16ed220383ffd4237652ca1e84fdc4
Marcos Vinicius <viniciusmr1499@gmail.com>
2020-12-01 20:49:11 -0300

    fixed issues

--- src/modules/appointments/services/ListProviderMonthAvailabilityService.spec.ts
+++ src/modules/appointments/services/ListProviderMonthAvailabilityService.spec.ts
@@ -1,97 +1,101 @@
 import FakeAppointmentsRepository from '@modules/appointments/repositories/fakes/FakeAppointmentsRepository';
-import ListProviderMonthAvailabilityService from './ListProviderMonthAvailabilityService';
+import ListProviderMonthAvailability from './ListProviderMonthAvailabilityService';
 
 let fakeAppointmentsRepository: FakeAppointmentsRepository;
-let listProviderMonthAvailability: ListProviderMonthAvailabilityService;
+let listProviderMonthAvailability: ListProviderMonthAvailability;
 
 describe('ListProviderMonthAvailability', () => {
-    beforeEach(() => {
-        fakeAppointmentsRepository = new FakeAppointmentsRepository(),
-        listProviderMonthAvailability = new ListProviderMonthAvailabilityService(
-            fakeAppointmentsRepository
-        );
+  beforeEach(() => {
+    fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    listProviderMonthAvailability = new ListProviderMonthAvailability(
+      fakeAppointmentsRepository,
+    );
+  });
+
+  it('should be able to list the month availability from providers', async () => {
+    await fakeAppointmentsRepository.create({
+      provider_id: 'user',
+      user_id: 'user',
+      date: new Date(2020, 4, 20, 8, 0, 0),
     });
 
-    it('Should be able to list the month availability from provider', async () => {
-        await fakeAppointmentsRepository.create({
-            provider_id: 'user',
-            user_id: 'user',
-            date: new Date(2020, 4, 20, 8, 0, 0),
-        });
-
-        await fakeAppointmentsRepository.create({
-            provider_id: 'user',
-            user_id: 'user',
-            date: new Date(2020, 4, 20, 9, 0, 0),
-        });
-
-        await fakeAppointmentsRepository.create({
-            provider_id: 'user',
-            user_id: 'user',
-            date: new Date(2020, 4, 20, 10, 0, 0),
-        });
-
-        await fakeAppointmentsRepository.create({
-            provider_id: 'user',
-            user_id: 'user',
-            date: new Date(2020, 4, 20, 11, 0, 0),
-        });
-
-        await fakeAppointmentsRepository.create({
-            provider_id: 'user',
-            user_id: 'user',
-            date: new Date(2020, 4, 20, 12, 0, 0),
-        });
-
-        await fakeAppointmentsRepository.create({
-            provider_id: 'user',
-            user_id: 'user',
-            date: new Date(2020, 4, 20, 13, 0, 0),
-        });
-
-        await fakeAppointmentsRepository.create({
-            provider_id: 'user',
-            user_id: 'user',
-            date: new Date(2020, 4, 20, 14, 0, 0),
-        });
-
-        await fakeAppointmentsRepository.create({
-            provider_id: 'user',
-            user_id: 'user',
-            date: new Date(2020, 4, 20, 15, 0, 0),
-        });
-
-        await fakeAppointmentsRepository.create({
-            provider_id: 'user',
-            user_id: 'user',
-            date: new Date(2020, 4, 20, 16, 0, 0),
-        });
-
-        await fakeAppointmentsRepository.create({
-            provider_id: 'user',
-            user_id: 'user',
-            date: new Date(2020, 4, 20, 17, 0, 0),
-        });
-
-        await fakeAppointmentsRepository.create({
-            provider_id: 'user',
-            user_id: 'user',
-            date: new Date(2020, 4, 21, 8, 0, 0),
-        });
-
-        const availability = await listProviderMonthAvailability.execute({
-            provider_id: 'user',
-            year: 2020,
-            month: 5,
-        });
-
-        expect(availability).toEqual(
-            expect.arrayContaining([
-                { day: 19, available: true },
-                { day: 20, available: false },
-                { day: 21, available: true },
-                { day: 22, available: true },
-            ]),
-        );
+    await fakeAppointmentsRepository.create({
+      provider_id: 'user',
+      user_id: 'user',
+      date: new Date(2020, 4, 20, 9, 0, 0),
     });
+
+    await fakeAppointmentsRepository.create({
+      provider_id: 'user',
+      user_id: 'user',
+      date: new Date(2020, 4, 20, 10, 0, 0),
+    });
+
+    await fakeAppointmentsRepository.create({
+      provider_id: 'user',
+      user_id: 'user',
+      date: new Date(2020, 4, 20, 11, 0, 0),
+    });
+
+    await fakeAppointmentsRepository.create({
+      provider_id: 'user',
+      user_id: 'user',
+      date: new Date(2020, 4, 20, 12, 0, 0),
+    });
+
+    await fakeAppointmentsRepository.create({
+      provider_id: 'user',
+      user_id: 'user',
+      date: new Date(2020, 4, 20, 13, 0, 0),
+    });
+
+    await fakeAppointmentsRepository.create({
+      provider_id: 'user',
+      user_id: 'user',
+      date: new Date(2020, 4, 20, 14, 0, 0),
+    });
+
+    await fakeAppointmentsRepository.create({
+      provider_id: 'user',
+      user_id: 'user',
+      date: new Date(2020, 4, 20, 15, 0, 0),
+    });
+
+    await fakeAppointmentsRepository.create({
+      provider_id: 'user',
+      user_id: 'user',
+      date: new Date(2020, 4, 20, 16, 0, 0),
+    });
+
+    await fakeAppointmentsRepository.create({
+      provider_id: 'user',
+      user_id: 'user',
+      date: new Date(2020, 4, 20, 17, 0, 0),
+    });
+
+    await fakeAppointmentsRepository.create({
+      provider_id: 'user',
+      user_id: 'user',
+      date: new Date(2020, 4, 21, 8, 0, 0),
+    });
+
+    jest.spyOn(Date, 'now').mockImplementation(() => {
+      return new Date(2020, 4, 1, 11).getTime();
+    });
+
+    const availability = await listProviderMonthAvailability.execute({
+      provider_id: 'user',
+      month: 5,
+      year: 2020,
+    });
+
+    expect(availability).toEqual(
+      expect.arrayContaining([
+        { day: 19, available: true },
+        { day: 20, available: false },
+        { day: 21, available: true },
+        { day: 22, available: true },
+      ]),
+    );
+  });
 });
